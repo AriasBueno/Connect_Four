a9e31c269e0db3718445a2e17c444dfef4ba711f
AriasBueno <madelinarias@mail.com>
2017-08-02 18:25:25 -0700

    Create Connect4_Interface.java

---
+++ Connect4_Interface.java
@@ -0,0 +1,11 @@
+package Game;
+
+public interface Connect4_Interface {
+	public void displayBoard();	
+	public void clearBoard();
+	public void displayWinner();
+	public void updateCurrPlayer();
+	public boolean isFull();
+
+
+}
